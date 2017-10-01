23c3a921770ed9c2f1fa7889a56928d6104c1d98
rw950431 <rw950431@gmail.com>
2017-10-01 22:17:17 +1030

    First upload- does it work?

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask import request
+
 application = Flask(__name__)
 
-@application.route("/")
+@application.route("/", methods=['GET', 'POST'])
 def hello():
+    token=request.args.get('token', 'not found')
     return "Hello World!"
+
+@application.route("/list/<password>")
+def show_list(password):
+    return "A list for {}".format(password)
 
 if __name__ == "__main__":
     application.run()
